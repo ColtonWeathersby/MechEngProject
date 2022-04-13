bae3d15b9e51d23af5bd82c031e6cfa447961ccb
ColtonWeathersby <Colton.weathersby4@gmail.com>
2022-04-13 16:04:30 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+
+const Create =()=>{
+  const [ArchDim, setArchDim] = useState('');
+  const [BBDim, setBBDim] = useState('')
+  const [Family,setFamily] = useState('')
+  const [Parent,setParent] = useState('')
+  const [Child, setChild] = useState('')
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const answers = {ArchDim, BBDim, Family, Parent, Child};
+    
+    console.log(answers)
+    localStorage.setItem('Answers',JSON.stringify(answers) );
+  }
+
+  return(
+    <div className="create">
+      <h2>Mech. Eng.</h2>
+      <form onSubmit={handleSubmit}> 
+        <label> Architecture Dimensionality </label>
+        <select
+        value={ArchDim}
+        onChange={(e) => setArchDim(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="1D">1D</option>
+          <option value="2D">2D</option>
+          <option value="2.5D">2.5D</option>
+          <option value="3D">3D</option>
+        </select>
+        <label>Building Block Dimensionality</label>
+        <select
+        value={BBDim}
+        onChange={(e)=> setBBDim(e.target.value)}
+        >
+          <option value="1D">1D</option>
+          <option value="2D">2D</option>
+          <option value="2.5D">2.5D</option>
+          <option value="3D">3D</option>
+        </select>
+        <label>Family</label>
+        <select
+        value={Family}
+        onChange={(e)=>setFamily(e.target.value)}
+        >
+          <option value="Network">Network</option>
+          <option value="Kirigami">Kirigami</option>
+          <option value="Origami">Origami</option>
+          <option value="Linkage">Linkage</option>
+          <option value="IPC">IPC</option>
+          <option value="Sandwich">Sandwich</option>
+          <option value="Other">Other</option>
+        </select>
+
+        <label for="parentCheck">Hierarchical Parent?:</label>
+        <input type="checkbox" id="parentCheck" value={Parent} onChange={(e)=>setParent(e.target.checked)} ></input>
+
+        <label for="childCheck"> Child?:</label>
+        <input type="checkbox" id="childCheck" onChange={(e)=>setChild(e.target.checked)}></input>
+
+      <button>Submit</button>
+      <button>About</button>
+      <p>Arch Dim:  {ArchDim}</p>
+      <p>BBDim:  {BBDim}</p>
+      <p>Family:    {Family}</p>
+      <p>Parent:  {Parent}</p>
+      <p>Child:   {Child}</p>
+      
+      
+      </form>
     </div>
   );
 }
 
-export default App;
+export default Create;
